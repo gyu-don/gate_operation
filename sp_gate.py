0e1ab2ae8b5f1fb73907df6226e82ac049962616
gyu-don <takumi.kt+git@gmail.com>
2018-01-05 17:03:33 +0900

    Delete unused functions and imports

--- sp_gate.py
+++ sp_gate.py
@@ -1,13 +1,24 @@
-import itertools
 import math
-import random
 import sympy
 from sympy.physics.quantum import TensorProduct
 
 from _gate_common import *
 
-
 class SymPyGateOperation(GateOperation):
+    _gate = build_gate_class(
+            'SymPyGate',
+            sympy.Matrix,
+            sympy.sqrt(2) / 2,
+            lambda a, b: a + b * sympy.I
+    )
+    _identity = staticmethod(sympy.eye)
+    _kron = staticmethod(TensorProduct)
+    _matmul = staticmethod(sympy.Matrix.multiply)
+
+    @staticmethod
+    def _inplace_multiply(mat1, mat2):
+        mat1 = mat1.multiply_elementwise(mat2)
+
     def __init__(self, n_bits, data, rng=None):
         self.n_bits = n_bits
         self.data = data
@@ -15,132 +26,6 @@ class SymPyGateOperation(GateOperation):
             self.rng = rng
         else:
             self.rng = random.Random()
-
-    def _make_single_gate_mat(self, gate, i):
-        if i > 0:
-            mat = TensorProduct(sympy.eye(2**i), gate)
-        else:
-            mat = gate
-        j = self.n_bits - i - 1
-        if j > 0:
-            mat = TensorProduct(mat, sympy.eye(2**j))
-        return mat
-
-    def apply_gate(self, gate, i):
-        self.data = self._make_single_gate_mat(gate, i) * self.data
-        return self
-
-    def u(self, gate_operation):
-        self.data = gate_operation.data * self.data
-        return self
-
-    def i(self, i):
-        return self
-
-    def h(self, i):
-        return self.apply_gate(_gate.h, i)
-
-    def x(self, i):
-        return self.apply_gate(_gate.x, i)
-
-    def y(self, i):
-        return self.apply_gate(_gate.y, i)
-
-    def z(self, i):
-        return self.apply_gate(_gate.z, i)
-
-    def s(self, i):
-        return self.apply_gate(_gate.s, i)
-
-    def t(self, i):
-        return self.apply_gate(_gate.t, i)
-
-    def s_dag(self, i):
-        return self.apply_gate(_gate.s_dag, i)
-
-    def t_dag(self, i):
-        return self.apply_gate(_gate.t_dag, i)
-
-    def apply_cgate(self, gate, c, i):
-        if c == i:
-            raise ValueError('Control bit and operation bit shall be different')
-        mat = self._make_single_gate_mat(gate, i)
-        mat = mat * self._make_single_gate_mat(_gate._one, c)
-        mat += self._make_single_gate_mat(_gate._zero, c)
-        self.data = mat * self.data
-        return self
-
-    def cu(self, c, gate_operation):
-        if c == i:
-            raise ValueError('Control bit and operation bit shall be different')
-        mat = self._make_single_gate_mat(_gate._one, c)
-        mat = gate_operation.data * mat
-        mat += self._make_single_gate_mat(_gate._zero, c)
-        self.data = mat * self.data
-        return self
-
-    def ci(self, c, i):
-        return self
-
-    def cx(self, c, i):
-        return self.apply_cgate(_gate.x, c, i)
-
-    def cy(self, c, i):
-        return self.apply_cgate(_gate.y, c, i)
-
-    def cz(self, c, i):
-        return self.apply_cgate(_gate.z, c, i)
-
-    def cs(self, c, i):
-        return self.apply_cgate(_gate.s, c, i)
-
-    def ct(self, c, i):
-        return self.apply_cgate(_gate.t, c, i)
-
-    def ch(self, c, i):
-        return self.apply_cgate(_gate.h, c, i)
-
-    def cs_dag(self, c, i):
-        return self.apply_cgate(_gate.s_dag, c, i)
-
-    def ct_dag(self, c, i):
-        return self.apply_cgate(_gate.t_dag, c, i)
-
-    cnot = cx
-
-    def swap(self, i, j):
-        return self.cx(i, j).cx(j, i).cx(i, j)
-
-    def apply_ccgate(self, gate, c1, c2, i):
-        if c1 == i or c2 == i:
-            raise ValueError('Control bit and operation bit shall be different')
-        if c1 == c2:
-            return self.apply_cgate(gate, c1, i)
-        mat = self._make_single_gate_mat(gate, i)
-        matc = self._make_single_gate_mat(_gate._one, c1)
-        matc *= self._make_single_gate_mat(_gate._one, c2)
-        mat = mat * matc
-        mat += sympy.eye(2**self.n_bits) - matc
-        self.data = mat * self.data
-        return self
-
-    def ccu(self, c1, c2, gate_operation):
-        if c1 == i or c2 == i:
-            raise ValueError('Control bit and operation bit shall be different')
-        if c1 == c2:
-            return self.cu(c1, gate_operation)
-        matc = self._make_single_gate_mat(_gate._one, c1)
-        matc *= self._make_single_gate_mat(_gate._one, c2)
-        mat = gate_operation.data * matc
-        mat += sympy.eye(2**self.n_bits) - matc
-        self.data = mat * self.data
-        return self
-
-    def ccnot(self, c1, c2, i):
-        return self.apply_ccgate(_gate.x, c1, c2, i)
-
-    toffoli = ccnot
-    ccx = ccnot
 
 
 class Qubit(SymPyGateOperation):
